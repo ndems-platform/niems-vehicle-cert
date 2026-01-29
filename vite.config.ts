d996ec32bb6bc2dc7e8765b89b4c10b94c5f851b
art-pontep <pontep.pre@gmail.com>
2026-01-29 11:27:59 +0700

    fix: Update Vite base path from `/niems-cert-vehicle/` to `/niems-vehicle-cert/`.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/niems-cert-vehicle/',
+    base: '/niems-vehicle-cert/',
     build: {
         outDir: 'dist',
         sourcemap: false,
